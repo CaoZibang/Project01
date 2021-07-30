47624a002df583d371a1e6a976e3956aa0f297ae
czb <2835207172@qq.com>
2021-07-30 13:12:25 +0800

    hot-fix commit

--- src/main/java/com/bjpowernode/App.java
+++ src/main/java/com/bjpowernode/App.java
@@ -10,5 +10,7 @@ public class App {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
         System.out.println("Hello Git! 22222");
+        System.out.println("Hello Git! hot-fix");
+
     }
 }
